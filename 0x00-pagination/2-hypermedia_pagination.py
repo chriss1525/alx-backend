6baf3e2d8ced82332bf3665fc66327a032af5bea
chriss1525 <okothchris15@gmail.com>
2023-10-24 17:03:48 +0300

    adding hypermedia controls to a class

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -47,11 +47,16 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """ return a dictionary containing key value pairs of the pagination"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
         return {
-            'page_size': len(self.get_page(page, page_size)),
+            'page_size': len(data),
             'page': page,
-            'data': self.get_page(page, page_size),
-            'next_page': page + 1 if page + 1 < len(self.dataset()) else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_pages': math.ceil(len(self.dataset()) / page_size)
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
         }
