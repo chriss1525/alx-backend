568698689304f1c77785056f322264ba4ac87226
chriss1525 <okothchris15@gmail.com>
2023-10-24 16:36:33 +0300

    simple pagination for a csv file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,6 +39,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """takes two integer arguments page with default value 1 and page_size
         with default value 10."""
+        if  not page or not page_size:
+            return []
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
