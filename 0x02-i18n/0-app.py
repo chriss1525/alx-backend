6254238144fce0f1e1991a5b3cd68f8a4982ce2c
chriss1525 <okothchris15@gmail.com>
2023-11-01 07:41:39 +0300

    import babel, configure the languages, lacale and timezone for our app to use

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,4 +9,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
+    """ render html file """
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask_babel import Babel
+from flask import *
+
+""" simple flask app that renders html file,\
+    import Babel and configure our app to use it """
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Configure available languages in our app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ render html file """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
