d629e2bb553ea1af5067ad27d87793536921c0ca
chriss1525 <okothchris15@gmail.com>
2023-11-01 08:33:17 +0300

    usinng the babel.localeselector decorator

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" simple flask app that renders html file,\
-import Babel and configure our app to use it """
+
+""" adding a get locale function with the babel.localeselector decorator"""
+
 from flask_babel import Babel
 from flask import *
 
@@ -23,7 +24,13 @@ app.config.from_object(Config)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ render simple hello world html file """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ get locale language code from request accepted languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
