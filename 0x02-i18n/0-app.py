1da62ef312894bab3f88b43245e3e0f672713f8e
chriss1525 <okothchris15@gmail.com>
2023-11-01 11:46:45 +0300

    fix styling

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-""" simple flask app that renders html file"""
+""" simple flask app that renders a basic html template """
+
 from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """ render simple hello world html file """
-    return render_template('0-index.html')
+def hello_world() -> str:
+    """ basic flask app route that renders a html file """
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
