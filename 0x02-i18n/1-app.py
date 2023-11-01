690f459ac960e461a0367e00a837fa67c7da472a
chriss1525 <okothchris15@gmail.com>
2023-11-01 08:41:13 +0300

    add default template folder

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-""" Module for trying out Babel i18n """
+# -*- coding: utf-8 -*-
+""" simple flask app that renders html file,\
+import Babel and configure our app to use it """
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import *
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
 class Config(object):
-    """ Configuration Class for Babel """
+    """ Configure available languages in our app """
 
-    LANGUAGES = ['en', 'fr']
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
@@ -19,10 +21,11 @@ app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """Renders a Basic Template for Babel Implementation"""
-    return render_template("1-index.html")
+def index() -> str:
+    """ render simple hello world html file """
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
     app.run()
+
